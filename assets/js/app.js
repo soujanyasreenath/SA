b8fe66b526bdac7c3a6af6b16c0393c4fbf4d638
soujanyasreenath <soujanya.sreenath@gmail.com>
2017-01-26 18:46:02 +0530

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,201 @@
+'use strict';
+var sapcart = angular.module("sapCart", []);
+
+sapcart.controller('cartController', ['$scope', '$http', '$window', function($scope, $http, $window) {
+  var cart_info;
+  $scope.edit = false;
+  $http.get("../assets/cart.json")
+  .then(function(response) {
+    $scope.cart = response["data"]["productsInCart"];
+    // local storage
+    $window.localStorage["cart_info"] = JSON.stringify(response["data"]["productsInCart"]);
+    // showing everything from local storage
+    $scope.myCart = JSON.parse($window.localStorage["cart_info"]);
+    // $scope.cart_total_items = $scope.myCart.length;
+    $scope.$watch(function() {
+      if ($scope.myCart != undefined) {
+        $scope.cart_total_items = $scope.myCart.length;
+      }
+    });
+
+    // edit modal
+    $scope.showEditModal = function(i) {
+      $scope.editModalShown = true;
+      $scope.edit = true;
+      $scope.productDetail = $scope.myCart[i];
+    }
+    $scope.hideEditModalCallback = function(dialogID) {
+      if (dialogID == 'editModalShown') {
+        $scope.editModalShown = false;
+      }
+    };
+
+    var product = 
+  { "productList":[  
+      {  
+         "p_id":"5",
+         "p_name":"paisley jean",
+         "p_image" : "../assets/P1.jpg",
+         "p_variation":"paisley",
+         "p_style":"ms13kt1906",
+         "p_selected_color":{  
+            "name":"cream",
+            "hexcode":"#f3f3db"
+         },
+         "p_selected_size":{  
+            "name":"small",
+            "code":"s"
+         },
+         "p_available_options":{  
+            "colors":[  
+               {  
+                  "name":"cream",
+                  "hexcode":"#f3f3db"
+               },
+               {  
+                  "name":"pista-green",
+                  "hexcode":"#a9d0a1"
+               }
+            ],
+            "sizes":[  
+               {  
+                  "name":"small",
+                  "code":"s"
+               },
+               {  
+                  "name":"medium",
+                  "code":"m"
+               },
+               {  
+                  "name":"large",
+                  "code":"l"
+               },
+               {  
+                  "name":"extra large",
+                  "code":"xl"
+               }
+            ]
+         },
+         "p_quantity":1,
+         "p_originalprice":21.0,
+         "p_price":21.0,
+         "c_currency":"$"
+      }
+    ]};
+
+    $scope.showModal = function() {
+      $scope.addModalShown = true;
+      $scope.productDetail = product["productList"][0];
+      $scope.edit = false;
+    }
+
+    $scope.hideModalCallback = function(dialogID) {
+      if (dialogID == 'addModalShown') {
+        $scope.addModalShown = false;
+      }
+    };
+
+    $scope.select = false;
+    
+    $scope.selectColor = function () {
+      $scope.select = true;
+    }
+    $scope.addToCart = function(i) {
+      // code
+      var selected_index = i;
+      $scope.myCart.push($scope.productDetail);
+      $window.localStorage["cart_info"] = JSON.stringify($scope.myCart);
+    }
+
+    $scope.editProduct = function() {
+      // code
+    }
+    $scope.removeProduct = function(i) {
+      // code
+      var selected_index = i;
+      $scope.myCart.splice(selected_index, 1);
+      $window.localStorage["cart_info"] = JSON.stringify($scope.myCart);
+      $scope.subTotal();
+    }
+
+    // finding subtotal
+    $scope.subTotal = function() {
+      // initiating variables
+      var st = 0;
+      var loop_item = 0;
+      var product_item = [];
+      if ($scope.myCart.length > 0) {
+        for (loop_item; loop_item < $scope.myCart.length; loop_item++) {
+          // iteration through each item in the hash - myCart
+          product_item = $scope.myCart[loop_item];
+          // finding subtotal st = st + (price*qty)
+          st += (product_item.p_price * product_item.p_quantity);
+        }
+      } else {
+        st = 0;
+      }
+      return st;
+    }
+
+    // discount logic
+    $scope.discountLogic = function() {
+      var discount = 0;
+      var subtotal = $scope.subTotal();
+      //  3 items in cart - 5% discount on subtotal amount
+      if ($scope.myCart.length === 3) {
+        return discount = subtotal * 0.5;
+      } 
+      // 3-6 items in cart - 10% discount on subtotal amount
+      else if ($scope.myCart.length > 3 && $scope.myCart.length <= 6) {
+        return discount = subtotal * 0.10;
+      } 
+      // Above 10 items in cart – 25% discount on subtotal amount
+      else if ($scope.myCart.length > 10) {
+        return discount = subtotal * 0.25;
+      } 
+      // handling other scenarios
+      else {
+        return discount;
+      }
+    }
+    if ($scope.myCart.length === 3) {
+      $scope.percentage = 5;
+    } else if ($scope.myCart.length > 3 && $scope.myCart.length <= 6) {
+      $scope.percentage = 10;
+    } else if ($scope.myCart.length > 10) {
+      $scope.percentage = 25;
+    } else {
+      $scope.percentage = 25;
+    }
+
+    // Finding estimated total
+    $scope.estTotal = $scope.subTotal() - $scope.discountLogic();
+  });
+}]);
+
+
+// modal box directive
+sapcart.directive('sapModal', [function() {
+  return {
+    restrict: 'E',
+    scope: {
+      show: '=',
+      onClosed :'&onClosed'
+    },
+    replace: true, // Replace with the template below
+    transclude: true, // we want to insert custom content inside the directive
+    link: function(scope, element, attrs) {
+      scope.dialogStyle = {};
+      if (attrs.width)
+        scope.dialogStyle.width = attrs.width;
+      if (attrs.height)
+        scope.dialogStyle.height = attrs.height;
+      scope.hideModal = function() {
+        scope.show = false;
+        scope.onClosed();
+      };
+    },
+    template : "<div class='ng-modal' ng-show='show'><div class='ng-modal-overlay' ng-click='hideModal()'></div><div class='ng-modal-dialog' ng-style='dialogStyle'><div class='ng-modal-close' ng-click='hideModal()'>X</div><div class='float-clear'></div><div class='ng-modal-dialog-content' ng-transclude></div></div></div>"
+  };
+}]);
+
---
+++ assets/js/readme.md
@@ -0,0 +1,39 @@
+# node js to be installed 
+ - Ubuntu / Mint: 
+	sudo apt-get update
+	sudo apt-get install nodejs
+	sudo apt-get install npm
+
+ - windows :
+ 	Install cygwin. (http://www.mcclean-cooper.com/valentino/cygwin_install/)
+	Use setup.exe in the cygwin folder to install the following packages:
+
+	devel → openssl
+	devel → g++-gcc
+	devel → make
+	python → python
+	devel → git
+	Open the cygwin command line with Start > Cygwin > Cygwin Bash Shell.
+
+	Run the below commands to download and build node.
+	cygwin_setup.sh
+	git clone git://github.com/ry/node.git
+	cd node
+	./configure
+	make
+	sudo make install
+
+ - Mac :
+ 	Install Xcode. (http://developer.apple.com/technologies/tools/)
+	Install git. (http://help.github.com/mac-git-installation/)
+	Run the following commands:
+	darwin_setup.sh
+	git clone git://github.com/ry/node.git
+	cd node
+	./configure
+	make
+	sudo make install
+
+# Access using http-server (you can install it using "npm install http-server"), since opening html documents directly on the browser will cause this error : "AngularJS Error: Cross origin requests are only supported for protocol schemes: http, data, chrome-extension, https"
+
+# click continue shopping for modal dialog to open and add product to bag.
